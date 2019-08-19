dbbb9e40104e1fa4cd775884b7201fc7da2896d0
aslam31319 <aslam31319@gmail.com>
2019-08-19 22:02:58 +0530

    Added nav bar component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import { NavBarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, NavBarComponent],
   imports: [
     CommonModule,
     LayoutRoutingModule
